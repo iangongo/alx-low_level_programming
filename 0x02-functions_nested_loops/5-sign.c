d6c1c6947dc105271417f5afa940f12fa883b666
iangongo <ianmwanyaegongo@gmail.com>
2023-10-11 15:56:42 -0700

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - this prints the sum
+ * @n: The number
+ * Return: 1 for all
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n < 0)
+{
+_putchar(45);
+return (-1);
+}
+else
+{
+_putchar(48);
+return (0);
+}
+}
