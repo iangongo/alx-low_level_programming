eb97bae3347bd26fa6f359df984884fe71cbb22b
iangongo <ianmwanyaegongo@gmail.com>
2023-10-23 16:07:17 -0700

    8. Double pointer, double fun

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * set_string - this is the main function name
+  *
+  * @s: Function parameter one
+  *
+  * @to: Function parameter two
+  *
+  * Return: Always 0.
+  */
+void set_string(char **s, char *to)
+
+{
+	*s = to;
+
+}
