e7dbf589d1b3e95ec80bff34f75aa05123835beb
iangongo <ianmwanyaegongo@gmail.com>
2023-10-23 15:41:47 -0700

    3. strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * _strspn - this is the main function name
+  *
+  * @s: Function parameter one
+  *
+  * @accept: Function parameter two
+  *
+  * Return: Always 0.
+  */
+unsigned int _strspn(char *s, char *accept)
+{
+
+	unsigned int _strspn(char *s, char *accept);
+	unsigned int i = 0;
+	int k;
+
+	while (*s)
+	{
+	for (k = 0; accept[k]; k++)
+	{
+	if (*s == accept[k])
+	{
+	i++;
+	break;
+	}
+	else if (accept[k + 1] == '\0')
+	return (i);
+	}
+	s++;
+	}
+	return (i);
+}
