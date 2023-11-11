193a015b197bf99df59bd94141a7d9caeecdecb2
iangongo <ianmwanyaegongo@gmail.com>
2023-11-10 18:54:41 -0800

    3. To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,53 +1,49 @@
 #include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
 
 /**
-* print_all - Prints all of the arguments when specified
-* @format: specifies the necessary operations
-* Return: void
-*/
+* print_all - prints anything
+* * @format: list of types of arguments passed to the function
+ */
+	void print_all(const char * const format, ...)
+	{
+	int i = 0;
+	char *str, *sep = "";
+	va_list list;
 
-void print_all(const char * const format, ...)
-{
-	int i;
-	int flag;
-	char *str;
-	va_list a_list;
+	va_start(list, format);
 
-	va_start(a_list, format);
-	i = 0;
-	while (format != NULL && format[i] != '\0')
+
+if (format)
+	{
+	while (format[i])
 	{
 		switch (format[i])
 		{
 			case 'c':
-				printf("%c", va_arg(a_list, int));
-				flag = 0;
+				printf("%s%c", sep, va_arg(list, int));
 				break;
 			case 'i':
-				printf("%i", va_arg(a_list, int));
-				flag = 0;
+				printf("%s%d", sep, va_arg(list, int));
 				break;
 			case 'f':
-				printf("%f", va_arg(a_list, double));
-				flag = 0;
+				printf("%s%f", sep, va_arg(list, double));
 				break;
 			case 's':
-				str = va_arg(a_list, char*);
-				if (str == NULL)
+				str = va_arg(list, char *);
+				if (!str)
 					str = "(nil)";
-				printf("%s", str);
-				flag = 0;
+				printf("%s%s", sep, str);
 				break;
 			default:
-				flag = 1;
-				break;
+				i++;
+				continue;
 		}
-		if (format[i + 1] != '\0' && flag == 0)
-			printf(", ");
+		sep = ", ";
 		i++;
 	}
+	}
 	printf("\n");
-	va_end(a_list);
+	va_end(list);
 }
-
-
